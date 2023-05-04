fcc11c09ddbe398c9ac26ddaf666a258fc382e45
Lucas Araujo <lucascdearaujo444@gmail.com>
2023-05-04 19:56:00 -0300

    navbar finalizada

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,33 @@
 import { SelectedPage } from "@/shared/types";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Navbar } from "./scenes/";
 
 function App() {
 	const [selectedPage, setSelectedPage] = useState<SelectedPage>(SelectedPage.Home);
+	const [isTopOfPage, setIsTopOfPage] = useState<boolean>(false);
+	console.log(isTopOfPage);
+
+	useEffect(() => {
+		const handleScroll = () => {
+			if (window.scrollY !== 0) setIsTopOfPage(false);
+			if (window.scrollY === 0) {
+				setIsTopOfPage(true);
+				setSelectedPage(SelectedPage.Home);
+				console.log(isTopOfPage);
+			}
+		};
+		window.addEventListener("scroll", handleScroll);
+
+		return () => window.removeEventListener("scroll", handleScroll);
+	}, []);
 
 	return (
 		<div className='app bg-gray-20'>
-			<Navbar selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+			<Navbar
+				selectedPage={selectedPage}
+				setSelectedPage={setSelectedPage}
+				isTopOfPage={isTopOfPage}
+			/>
 		</div>
 	);
 }
--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -4,23 +4,25 @@ import { iMenus } from "@/interfaces/navInterfaces";
 import { ActionButton } from "@/shared/components";
 import { SelectedPage } from "@/shared/types";
 import { menus } from "@/utils/navLinks";
-import { Bars3Icon } from "@heroicons/react/24/solid";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
 import { useState } from "react";
 import Link from "./Link";
 
 type Props = {
 	selectedPage: SelectedPage;
 	setSelectedPage: (page: SelectedPage) => void;
+	isTopOfPage: boolean;
 };
 
-export default function Navbar({ selectedPage, setSelectedPage }: Props) {
+export default function Navbar({ selectedPage, setSelectedPage, isTopOfPage }: Props) {
 	const flexBetween = "flex items-center justify-between";
 	const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)");
 	const [isMenuToggled, setIsMenuToggled] = useState<boolean>(false);
+	const navbarBackground = isTopOfPage ? "" : "bg-primary-100 drop-shadow";
 
 	return (
 		<nav>
-			<div className={`${flexBetween} fixed top-0 z-30 w-full py-6`}>
+			<div className={`${navbarBackground} ${flexBetween} fixed top-0 z-30 w-full py-6`}>
 				<div className={`${flexBetween} mx-auto w-5/6`}>
 					<div className={`${flexBetween} w-full gap-16`}>
 						<img src={Logo} alt='Logo' />
@@ -45,7 +47,7 @@ export default function Navbar({ selectedPage, setSelectedPage }: Props) {
 							</div>
 						) : (
 							<button
-								onClick={() => setIsMenuToggled(isMenuToggled)}
+								onClick={() => setIsMenuToggled(!isMenuToggled)}
 								className='rounded-full bg-secondary-500 p-2'>
 								<Bars3Icon className='h-6 w-6 text-white' />
 							</button>
@@ -53,6 +55,28 @@ export default function Navbar({ selectedPage, setSelectedPage }: Props) {
 					</div>
 				</div>
 			</div>
+			{/* Mobile Menu Modal */}
+			{!isAboveMediumScreens && isMenuToggled && (
+				<div className='fixed bottom-0 right-0 z-40 h-full w-[300px] bg-primary-100 drop-shadow-xl'>
+					{/* Close Icon */}
+					<div className='flex justify-end p-12'>
+						<button onClick={() => setIsMenuToggled(!isMenuToggled)}>
+							<XMarkIcon className='h-6 w-6 text-gray-400' />
+						</button>
+					</div>
+					{/* Menu Items */}
+					<div className='ml-[33%] flex flex-col gap-10 text-2xl'>
+						{menus.map((menu: iMenus) => (
+							<Link
+								key={menu.id}
+								page={menu.name}
+								selectedPage={selectedPage}
+								setSelectedPage={setSelectedPage}
+							/>
+						))}
+					</div>
+				</div>
+			)}
 		</nav>
 	);
 }
